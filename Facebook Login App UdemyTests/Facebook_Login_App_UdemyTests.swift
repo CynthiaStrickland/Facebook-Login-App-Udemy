226b8bdaca0bc83fc5717ddb1b947ca055f6f548
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-12-26 19:56:40 -0800

    Initial Commit

---
+++ Facebook Login App UdemyTests/Facebook_Login_App_UdemyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Facebook_Login_App_UdemyTests.swift
+//  Facebook Login App UdemyTests
+//
+//  Created by Cynthia Whitlatch on 12/26/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import XCTest
+@testable import Facebook_Login_App_Udemy
+
+class Facebook_Login_App_UdemyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
