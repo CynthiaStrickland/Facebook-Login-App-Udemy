ef2996c96634829fc820f3c210c4243f9a25df74
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-01-12 14:31:09 -0800

    Frameworks & UI

--- Facebook Login App Udemy/ViewController.swift
+++ Facebook Login App Udemy/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var profilePictureView: FBSDKProfilePictureView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton: FBSDKLoginButton = FBSDKLoginButton()
+        loginButton.center = CGPoint(x: CGRectGetMidX(self.view.frame), y: CGRectGetHeight(self.view.frame)-loginButton.frame.height)
+        self.view.addSubview(loginButton)
+        
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            statusLabel.text = "You are logged in as"
+            
+        displayName()
+            
+        } else {
+            statusLabel.text = "You are logged out!"
+            nameLabel.text = ""
+        }
+    }
+    
+    func displayName() {
+        
+        let graphRequest: FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "Me", parameters: nil)
+        
+        graphRequest.startWithCompletionHandler { (FBSDKGraphRequestConnection, result, error) -> Void in
+            
+            let name = result.valueForKey("name") as! String
+            self.nameLabel.text = name
+            
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
